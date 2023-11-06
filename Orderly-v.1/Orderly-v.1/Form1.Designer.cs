6291f487618f1247d96357d04cb332a0c4e845b4
Bradley <brw2010@live.co.za>
2023-11-06 20:28:23 +0200

    Login form

---
+++ Orderly-v.1/Orderly-v.1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Orderly_v._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            textBox_username = new TextBox();
+            textBox_password = new TextBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(42, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(42, 230);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // textBox_username
+            // 
+            textBox_username.Location = new Point(109, 164);
+            textBox_username.Name = "textBox_username";
+            textBox_username.Size = new Size(208, 23);
+            textBox_username.TabIndex = 2;
+            // 
+            // textBox_password
+            // 
+            textBox_password.Location = new Point(109, 222);
+            textBox_password.Name = "textBox_password";
+            textBox_password.Size = new Size(208, 23);
+            textBox_password.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(76, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(241, 105);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(232, 266);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 28);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(401, 407);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox_password);
+            Controls.Add(textBox_username);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox_username;
+        private TextBox textBox_password;
+        private PictureBox pictureBox1;
+        private Button button1;
+    }
+}
---
+++ Orderly-v.1/Orderly-v.1/Form1.cs
@@ -0,0 +1,20 @@
+namespace Orderly_v._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
